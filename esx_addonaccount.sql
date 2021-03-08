c8eb126a6e22848ecf208f3d818dfcabbe960063
Pierre Pellegrini <pellegrini.pierre@gmail.com>
2021-03-08 07:59:10 +0100

    Maj sql pour le français

--- esx_addonaccount.sql
+++ esx_addonaccount.sql
@@ -8,6 +8,18 @@ CREATE TABLE `addon_account` (
 	PRIMARY KEY (`name`)
 );
 
+INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
+('bank_savings', 'Bank Savings', 0),
+('caution', 'caution', 0),
+('property_black_money', 'Argent Sale Propriété', 0),
+('society_ambulance', 'Ambulance', 1),
+('society_banker', 'Banker', 1),
+('society_cardealer', 'Cardealer', 1),
+('society_mechanic', 'Mechanic', 1),
+('society_police', 'Police', 1),
+('society_realestateagent', 'Agent immobilier', 1),
+('society_taxi', 'Taxi', 1);
+
 CREATE TABLE `addon_account_data` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`account_name` VARCHAR(100) DEFAULT NULL,
